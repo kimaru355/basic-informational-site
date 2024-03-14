e1ae22db4845e487dea93cfe5ee14942f7cdb37c
kimaru355 <emmanuelkimaru355@gmail.com>
2024-03-14 21:15:30 +0300

    basic server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+
+const port = 8080;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).sendFile(path.join(module.path, "/public/index.html"));
+});
+app.get("/about", (req, res) => {
+  res.status(200).sendFile(path.join(module.path, "/public/about.html"));
+});
+app.get("/contact-me", (req, res) => {
+  res.status(200).sendFile(path.join(module.path, "/public/contact-me.html"));
+});
+app.get("/*", (req, res) => {
+  res.status(200).sendFile(path.join(module.path, "/public/404.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
