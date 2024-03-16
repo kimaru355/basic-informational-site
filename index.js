8c4f6bbbbe9d6e87ea76b941f21058055d038be0
kimaru355 <emmanuelkimaru355@gmail.com>
2024-03-16 21:18:34 +0300

    basic informational site

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 
-const port = 8080;
+const port = process.env.port || 8080;
 const app = express();
 
 app.get("/", (req, res) => {
